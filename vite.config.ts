3d38a7d4903745454772b7f5ba495e6b9592b80c
ekrin1 <kottovakatya@mail.ru>
2025-08-23 21:28:39 +0300

    up

--- vite.config.ts
+++ vite.config.ts
@@ -2,6 +2,6 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/shop-2.3.2/',
   plugins: [react()],
-  base: '/shop/', // 
 })
